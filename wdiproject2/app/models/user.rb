865fc90c1d3bc038155c7ccb7d9b65a78dd8ac76
Evon <fumblies@gmail.com>
2016-06-27 14:54:43 +0800

    user login

--- wdiproject2/app/models/user.rb
+++ wdiproject2/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, presence: true, uniqueness: true, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, message: ': Email is invalid' }
-    
-    validates :password, presence: true, length: { minimum: 8 }, on: :create
+
+    validates :password, presence: true, length: { minimum: 8 }
 
 end
--- wdiproject2/config/routes.rb
+++ wdiproject2/config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :sessions, only:[:new, :create]
+  resources :sessions, only:[:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
