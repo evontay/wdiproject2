75f902b261fc19e934319d686b28d201a0088be0
Evon <fumblies@gmail.com>
2016-06-27 13:52:16 +0800

    passed email validation test

--- wdiproject2/app/models/user.rb
+++ wdiproject2/app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_many :posts
     has_secure_password
 
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, message: ': Email is invalid' }
+    
     validates :password, presence: true, length: { minimum: 8 }, on: :create
 
 end
