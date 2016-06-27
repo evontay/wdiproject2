82189f2ca1d5132d90f1209bd918c7ecc0de8dc9
Evon <fumblies@gmail.com>
2016-06-27 13:35:46 +0800

    initial commit

---
+++ wdiproject2/app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      redirect_to root_path, notice: "Logged In"
+    else
+      flash.now.alert = "invalid login details" #alert immediately rather than at the next session
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged Out"
+  end
+end
